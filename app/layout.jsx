c48fb0a0ff04d304690f0e0746e72620c3eed31b
Limmek <jimaugustsson@gmail.com>
2024-05-29 15:45:20 +0200

    Added dependabot and analytics v4

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Inter } from 'next/font/google'
 import '@/style/globals.css'
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
 			<body className='min-h-screen mx-auto flex flex-col w-auto'>
+				<GoogleAnalytics gaId="G-88BNTNNS69" />
 				<ThemeProvider>
 					<ParticleEffect className='fixed h-full w-full top-0 left-0 m-0 p-0 -z-10' />
 					<Navbar />
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
-    basePath: '/www',
+    basePath: '',
 };
 
 export default nextConfig;
