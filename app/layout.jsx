3ca6591a6407530d6f8b3663d2b1489691d17988
Limmek <jimaugustsson@gmail.com>
2024-05-31 23:13:09 +0200

    added google analytics to env

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
 			<body className='min-h-screen mx-auto flex flex-col w-auto'>
-				<GoogleAnalytics gaId="G-88BNTNNS69" />
+				<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
 				<ThemeProvider>
 					<ParticleEffect className='fixed h-full w-full top-0 left-0 m-0 p-0 -z-10' />
 					<Navbar />
